d7b5bb19d209e1630de8504ad865b5cff71a2f8c
SCREECH115 <arekw99@gmail.com>
2023-03-04 20:48:00 +0100

    Fixed: SSN number displaying date of birth and random digits, Fixed: Copy to clipboard, Addded: Places, Business, Money, Car, page got some style now

---
+++ tipply.js
@@ -0,0 +1,90 @@
+tippy("#firstNameTooltip", {
+  content: "Copy first name!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#lastNameTooltip", {
+  content: "Copy last name!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#firstAndLastNameTooltip", {
+  content: "Copy first and last name!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#peselTooltip", {
+  content: "Copy PESEL!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#dateOfBirthTooltip", {
+  content: "Copy date of birth!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#idNumberTooltip", {
+  content: "Copy ID number!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#codepostTooltip", {
+  content: "Copy code post!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#townTooltip", {
+  content: "Copy town!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#streetTooltip", {
+  content: "Copy street!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#nipTooltip", {
+  content: "Copy employer ID number!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#regonTooltip", {
+  content: "Copy EID!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#companyNameTooltip", {
+  content: "Copy company name!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#cardNumberTooltip", {
+  content: "Copy card number!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#bankNameTooltip", {
+  content: "Copy bank name!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#bankNumberTooltip", {
+  content: "Copy bank number!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#brandTooltip", {
+  content: "Copy car brand!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#carNumberTooltip", {
+  content: "Copy car number!",
+  placement: "bottom",
+  arrow: true,
+});
+tippy("#carVinTooltip", {
+  content: "Copy VIN number!",
+  placement: "bottom",
+  arrow: true,
+});
